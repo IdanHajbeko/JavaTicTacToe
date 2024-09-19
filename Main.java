1ca9a2a450776702e768de1083b36db235974365
Idan Hajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-09-19 18:33:09 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     static private final Random random = new Random();
     static public boolean isXTurn = random.nextBoolean();
     static public int[][] game = new int[3][3];
-    static public JPanel  panel = new JPanel();
+    static public JPanel panel = new JPanel();
+    static public boolean isAgainstAI = false;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -26,6 +26,12 @@ public class Main {
         panel.setLayout(new GridLayout(3, 3, 10, 10));
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
+        String[] options = {"Against Friend", "Against AI"};
+        int choice = JOptionPane.showOptionDialog(null, "Choose your game mode", "Tic Tac Toe",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+        isAgainstAI = (choice == 1);
+
         for (int i = 0; i < 9; i++) {
             JButton button = new JButton();
             button.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -42,14 +48,14 @@ public class Main {
         frame.setVisible(true);
     }
 
-    static void winner(int theWinner){
-        JOptionPane.showMessageDialog(null, (((theWinner == 1) ? "X" : "O") + " Won!!!!") ,"we got a winner", JOptionPane.PLAIN_MESSAGE);
+    static void winner(int theWinner) {
+        JOptionPane.showMessageDialog(null, (((theWinner == 1) ? "X" : "O") + " Won!!!!"), "we got a winner", JOptionPane.PLAIN_MESSAGE);
         resetBoard();
     }
 
-    static void resetBoard(){
-        for (Component comp : panel.getComponents()){
-            if (comp instanceof JButton button){
+    static void resetBoard() {
+        for (Component comp : panel.getComponents()) {
+            if (comp instanceof JButton button) {
                 button.setText("");
                 button.setEnabled(true);
             }
@@ -58,53 +64,68 @@ public class Main {
         game = new int[3][3];
     }
 
-    static void checkForWinners(){
-        for (int i = 0; i < 3; i++){
-            if (game[i][0] == game[i][1] && game[i][1] == game[i][2] && game[i][0] != 0){
+    static void checkForWinners() {
+        for (int i = 0; i < 3; i++) {
+            if (game[i][0] == game[i][1] && game[i][1] == game[i][2] && game[i][0] != 0) {
                 winner(game[i][0]);
             }
-            if (game[0][i] == game[1][i] && game[2][i] == game[1][i] && game[0][i] != 0){
+            if (game[0][i] == game[1][i] && game[2][i] == game[1][i] && game[0][i] != 0) {
                 winner(game[0][i]);
             }
         }
-        if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && game[0][0] != 0){
+        if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && game[0][0] != 0) {
             winner(game[0][0]);
         }
-        if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && game[0][2] != 0){
+        if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && game[0][2] != 0) {
             winner(game[0][2]);
         }
     }
 
-    static void checkForDraw(){
+    static void checkForDraw() {
         boolean allFilled = true;
-        for (int[] i: game){
-            for(int j: i){
-                if (j == 0){
+        for (int[] i : game) {
+            for (int j : i) {
+                if (j == 0) {
                     allFilled = false;
                     break;
                 }
             }
         }
-        if (allFilled){
-            JOptionPane.showMessageDialog(null, "Its a draw" ,"its a draw", JOptionPane.PLAIN_MESSAGE);
+        if (allFilled) {
+            JOptionPane.showMessageDialog(null, "It's a draw", "It's a draw", JOptionPane.PLAIN_MESSAGE);
             resetBoard();
         }
     }
 
-    static void placePiece(JButton button, int place){
+    static void placePiece(JButton button, int place) {
         button.setEnabled(false);
-        int row = place/3;
+        int row = place / 3;
         int col = place % 3;
         game[row][col] = isXTurn ? 1 : 2;
         button.setText(isXTurn ? "X" : "O");
         isXTurn = !isXTurn;
+
         checkForWinners();
         checkForDraw();
-        Move bestMove = MinMaxAlgorithm.findBestMove(game, (isXTurn) ? 1 : 2, (isXTurn) ? 2 : 1);
-        System.out.println(bestMove.row + " " + bestMove.col);
-        System.out.println(Arrays.deepToString(game));
 
+        if (isAgainstAI && !isXTurn) {
+            aiMove();
+        }
     }
 
+    static void aiMove() {
+        Move bestMove = MinMaxAlgorithm.findBestMove(game, 2, 1);
+        if (bestMove != null) {
+            game[bestMove.row][bestMove.col] = 2;
+            Component comp = panel.getComponent(bestMove.row * 3 + bestMove.col);
+            if (comp instanceof JButton button) {
+                button.setText("O");
+                button.setEnabled(false);
+            }
+        }
 
+        isXTurn = !isXTurn;
+        checkForWinners();
+        checkForDraw();
+    }
 }
